4b7969a835de2043b050fa75b43e708e484e839b
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2022-08-11 22:39:47 +0900

    Test

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/")
     public String hello(){
-        return "hello Load balancer!!";
+        return "test123";
     }
 
 }
